d04880fdcf52fd0af416ce40e864910c9efaf0d2
calilisantos <calilisantos@gmail.com>
2025-06-04 00:14:14 -0300

    shell loop version 2

--- shell_loops.sh
+++ shell_loops.sh
@@ -1,51 +1,40 @@
-# log_file="move_store.log"
-staging_layer="staging/*"
-files=$(for file in $staging_layer; do printf "%s " "$file"; done)
+#!/bin/bash
+staging_env="staging"
+dev_env="dev"
+hml_env="hml"
+prod_env="prod"
+working_dir="*"
+log_file="move_store.log"
+layers=$(for layer in $working_dir; do if [ -d $layer ]; then printf "%s " "$layer"; fi done)
 
-echo "$files"
+for file in $(find $working_dir -name *.json); do
+  file_env=$(jq -r '.properties.environment.value' "$file") # -r is jq --raw-output
+  file_name=$(basename "$file")
+  dir_name=$(dirname "$file")
 
-# dev_env="dev"
-# hml_env="hml"
-# prod_env="prod"
-
-# ingestion_flow() {
-#   file=$1
-#   file_env=$(jq -r '.properties.environment.value' "$file") # -r is jq --raw-output
-#   file_name=$(basename "$file")
-
-#   if test "$file_env" = "$dev_env"; then # checking environment passed
-#     echo "$(date "+%Y-%m-%d %T") [INFO] Processing "$file" in "$dev_env""
-#   elif [ "$file_env" = "$hml_env" ]; then
-#     echo "$(date "+%Y-%m-%d %T") [INFO] Processing "$file" in "$hml_env""
-#   elif [ "$file_env" = "$prod_env" ]; then
-#     echo "$(date "+%Y-%m-%d %T") [INFO] Processing "$file" in "$prod_env""
-#   else
-#     invalid_env_message="$(date "+%Y-%m-%d %T") [ERROR] Invalid environment in "$file""
-#     echo "$invalid_env_message" >> "$log_file" # creating failed log
-#     echo "$invalid_env_message"
-#     exit 1
-#   fi
-
-#   case $file_env in # move the file to the right environment
-#     $dev_env)
-#       cp "$file" "$dev_env"
-#     ;;
-#     $hml_env)
-#       cp "$file" "$hml_env"
-#     ;;
-#     *)
-#       cp "$file" "$prod_env"
-#     ;;
-#   esac
-#   processed_message="$(date "+%Y-%m-%d %T") [INFO] "$file_name" processed in $file_env"
-#   echo $processed_message >> "$log_file" # creating success log
-#   echo $processed_message
-
-#   rm -f "$file"  # removing staging file
-#   echo "$(date "+%Y-%m-%d %T") [INFO] "$file_name" cleaned in staging layer."
-# }
-
-# ingestion_flow staging/run_01.json
-# ingestion_flow staging/run_02.json
-# ingestion_flow staging/run_03.json
-# ingestion_flow staging/run_04.json
+  if [[ " $layers " =~ " $file_env " && "$file_env" = "$dir_name" ]]; then
+    echo "$(date "+%Y-%m-%d %T") [INFO] Starting the ingestion of "$file_name" in "$file_env""
+    echo "$(date "+%Y-%m-%d %T") [INFO] Processing "$file_name" in "$file_env""
+    case $file_env in # move the file to the next environment
+      $staging_env)
+        jq --arg next_env "$dev_env" '.properties.environment.value = $next_env' "$file" > tmp && mv tmp "$file"
+        mv "$file" "$dev_env/"
+      ;;
+      $dev_env)
+        jq --arg next_env "$hml_env" '.properties.environment.value = $next_env' "$file" > tmp && mv tmp "$file"
+        mv "$file" "$hml_env/"
+      ;;
+      $hml_env)
+        jq --arg next_env "$prod_env" '.properties.environment.value = $next_env' "$file" > tmp && mv tmp "$file"
+        mv "$file" "$prod_env/"
+      ;;
+    esac
+    processed_message="$(date "+%Y-%m-%d %T") [INFO] "$file_name" processed in $file_env"
+    echo $processed_message >> "$log_file" # creating success log
+    echo $processed_message
+  else
+    invalid_env_message="$(date "+%Y-%m-%d %T") [ERROR] Invalid environment in "$file_name""
+    echo "$invalid_env_message" >> "$log_file" # creating failed log
+    echo "$invalid_env_message"
+  fi
+done
